b2209f03648fa08b84a263a84a3a60da6d58d1ef
Rohit Patidar <patidar_rohit@yahoo.co.in>
2023-10-24 16:41:02 -0400

    added certificate

--- themalwas_project/build.sh
+++ themalwas_project/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/1d87007b-9a15-465c-8ea0-070c2556faec/cert'
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
