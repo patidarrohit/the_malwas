870fa472024fe0a21e5e2dc317b0f8bc3db70915
Rohit Patidar <patidar_rohit@yahoo.co.in>
2023-10-24 17:03:01 -0400

    test changes

--- themalwas_project/build.sh
+++ themalwas_project/build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/1d87007b-9a15-465c-8ea0-070c2556faec/cert'
+ls -lrt $HOME/.postgresql/root.crt
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
